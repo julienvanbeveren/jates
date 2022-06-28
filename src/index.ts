d9ffd2223f7915e0388925564b910a301c897439
julienvanbeveren <julien.vanbeveren@gmail.com>
2022-06-28 13:40:22 +0200

    month formatting ready

--- src/index.ts
+++ src/index.ts
@@ -13,4 +13,80 @@ export default class Jate extends Date {
         return type == 'short' ? this.daysShort[this.getDay()] : this.days[this.getDay()]
     }
 
+    format(formatter: string) {
+        let tokens = []
+        let nextToken = ''
+        for (let i = 0; i < formatter.length; i++) {
+            if (formatter[i].match(/\W/)) {
+                if (nextToken) {
+                    tokens.push(nextToken)
+                    nextToken = ''
+                }
+                tokens.push(formatter[i])
+            }
+            else {
+                nextToken = nextToken + formatter[i]
+                if (i > formatter.length - 2) {
+                    tokens.push(nextToken)
+                }
+            }
+        }
+        console.log(tokens)
+
+        let formattedDate = ''
+        for (const token of tokens) {
+            if (token.match(/\W/)) {
+                formattedDate += token
+                continue
+            }
+            switch (token) {
+                // Month formatting
+                case 'MMMMM':
+                    formattedDate += this.months[this.getMonth()][0]; break
+                case 'MMMM':
+                    formattedDate += this.months[this.getMonth()]; break
+                case 'MMM':
+                    formattedDate += this.monthsShort[this.getMonth()]; break
+                case 'MM':
+                    formattedDate += this.#formatNumber(this.getMonthNumber()); break
+                case 'M': 
+                    formattedDate += this.getMonthNumber().toString(); break
+                case 'Mo':
+                    formattedDate += this.#getOrdinalNumber(this.getMonthNumber()); break
+            }
+        }
+    }
+
+    #getNextMonthNumber() {
+        return this.getMonth() + 1 > 11 ? 0 : this.getMonth() + 1
+    }
+
+    getMonthNumber() {
+        return this.getMonth() + 1 > 12 ? 1 : this.getMonth() + 1
+    }
+
+    #formatNumber(number: number) {
+        const numberString = number.toString()
+        return numberString.length > 1 ? numberString : '0' + numberString
+    }
+
+    #getOrdinalNumber(number: number) {
+        const ordinals = ['st', 'nd', 'rd', 'th']
+        if (parseInt(number.toString().slice(-1)) == 1) { return number.toString() + 'st' }
+        if (parseInt(number.toString().slice(-1)) == 2) { return number.toString() + 'nd' }
+        if (parseInt(number.toString().slice(-1)) == 3) { return number.toString() + 'rd' }
+        if (parseInt(number.toString().slice(-1)) == 4) { return number.toString() + 'th' }
+        if (parseInt(number.toString().slice(-1)) == 5) { return number.toString() + 'th' }
+        if (parseInt(number.toString().slice(-1)) == 6) { return number.toString() + 'st' }
+        if (parseInt(number.toString().slice(-1)) > 6 && parseInt(number.toString().slice(-1)) < 10) { return number.toString() + 'th' }
+        if (number > 10 && number < 20) { return number.toString() + 'th' }
+        if (number % 10 == 0) { return number.toString() + 'th' }
+    }
+
 }
+
+let myDate = new Jate()
+myDate.format('Mo')
+
+
+
