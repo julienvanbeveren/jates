5247b3960291e3fe09108ad63b149a8e40423cff
julienvanbeveren <julien.vanbeveren@gmail.com>
2022-06-28 11:46:46 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+export class Jate extends Date {
+
+    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    monthsShort = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
+    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    daysShort = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+
+    getMonthName(type: 'long' | 'short' = 'long') {
+        return type == 'short' ? this.monthsShort[this.getMonth()] : this.months[this.getMonth()]
+    }
+
+    getDayName(type: 'long' | 'short' = 'long') {
+        return type == 'short' ? this.daysShort[this.getDay()] : this.days[this.getDay()]
+    }
+
+}
